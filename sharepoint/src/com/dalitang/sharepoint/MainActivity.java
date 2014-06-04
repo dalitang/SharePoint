441bbc6cec6fa2354664697838bb690d830e1e57
dalitang <dalitang@hotmail.com>
2014-06-04 23:10:37 +1000

    Add some buttons
    Signed-off-by: dalitang <dalitang@hotmail.com>

--- sharepoint/src/com/dalitang/sharepoint/MainActivity.java
+++ sharepoint/src/com/dalitang/sharepoint/MainActivity.java
@@ -1,18 +1,22 @@
 package com.dalitang.sharepoint;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
+	int counter;
+	Button add, sub;
+	TextView display;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +27,12 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+        
+        //ADD "view" Xml connection with "controller"
+        counter=0;
+        add=(Button) findViewById(R.id.bAdd);
+        sub=(Button) findViewById(R.id.bSub);
+        display = (TextView) findViewById(R.id.tvDisplay);
     }
 
 
